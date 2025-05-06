966b2c6c6738fb64a5a70bae676adea7ca77cac1
Lucas Rodrigues <lucas_luri@hotmail.com>
2025-05-05 23:20:22 -0300

    feat: Testes Unitarios

---
+++ Test_API_100k_users/Test1.cs
@@ -0,0 +1,32 @@
+﻿namespace Test_API_100k_users { 
+using Codecon_API_100k_users;
+using Codecon_API_100k_users.Data;
+using Codecon_API_100k_users.Services;
+
+    [TestClass]
+    public sealed class Test_API_100k_users 
+    {
+        // Arrange
+        List<User> user = new List<User> {
+                new User {
+                    Nome = "João",
+                    Idade = 25,
+                    Score = 950,
+                    Ativo = true,
+                    Pais = "Brasil"
+                }
+            };
+        UserServices userService = new UserServices();
+
+        [TestMethod]
+        public void TestPostUsers()
+        {
+
+            // Act
+            List<User> result = userService.PostUsers(user);
+            Assert.IsNotNull(result[0]);
+            Assert.AreEqual("João", result[0].Nome);
+
+        }
+    }
+}
