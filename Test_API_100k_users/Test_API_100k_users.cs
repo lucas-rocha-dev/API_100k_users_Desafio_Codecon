bf5a08d1444f2fea549669a8f3d3f414f3a98829
Lucas Rodrigues <lucas_luri@hotmail.com>
2025-05-06 22:32:16 -0300

    implementação dos testes finalizada

--- Test_API_100k_users/Test_API_100k_users.cs
+++ Test_API_100k_users/Test_API_100k_users.cs
@@ -1,6 +1,7 @@
 ﻿namespace Test_API_100k_users { 
 using Codecon_API_100k_users.Data;
-using Codecon_API_100k_users.Services;
+    using Codecon_API_100k_users.Dto;
+    using Codecon_API_100k_users.Services;
 
     [TestClass]
     public sealed class Test_API_100k_users 
@@ -12,9 +13,22 @@ using Codecon_API_100k_users.Services;
                     Idade = 25,
                     Score = 950,
                     Ativo = true,
-                    Pais = "Brasil"
+                    Pais = "Brasil",
+                    Equipe = new Equipe {
+                        Nome = "Equipe A",
+                        Lider = true,
+                        Projetos = new List<Projetos> {
+                            new Projetos { Nome = "Projeto 1", Concluido = true },
+                            new Projetos { Nome = "Projeto 2", Concluido = false }
+                        }
+                    },
+                    Logs = new List<Log> {
+                        new Log { Data = new DateTime(1991, 08, 20), Acao = "login" },
+                        new Log { Data =  new DateTime(2007, 10, 07), Acao = "logout" }
+                    }
                 }
             };
+
         private IUserService _userService = new UserServices();
 
         [TestMethod]
@@ -36,5 +50,35 @@ using Codecon_API_100k_users.Services;
             Assert.AreEqual("João", result[0].Nome);
 
         }
+
+        [TestMethod]
+        public void TopCountries()
+        {
+            // Act
+            List<TopCountriesDto> result = _userService.TopCountries(user);
+            Assert.IsNotNull(result[0]);
+            Assert.AreEqual("Brasil", result[0].Pais);
+        }
+
+        [TestMethod]
+        public void TeamInsights()
+        {
+            // Act
+            List<TeamInsightsDto> result = _userService.TeamInsights(user);
+            Assert.IsNotNull(result[0]);
+            Assert.AreEqual("Equipe A", result[0].Nome);
+
+        }
+        [TestMethod]
+        public void ActiveUsersPerDay()
+        {
+            // Act
+            List<ActivePerDayDto> result = _userService.ActiveUsersPerDay(user,1);
+            Assert.IsNotNull(result[0]);
+            Assert.AreEqual(new DateTime(1991, 08, 20), result[0].Date);
+
+        }
+
+
     }
 }
